24266bfbfc09becd63bf61b90642ac4e788e2ad0
Omar Cusma Fait <31404553+Omega97@users.noreply.github.com>
2025-04-05 10:31:16 +0200

    first upload

---
+++ main.py
@@ -0,0 +1,10 @@
+import sys
+import numpy
+import matplotlib
+import torch
+
+
+print(sys.version)
+print(f"NumPy version: {numpy.__version__}")
+print(f"Matplotlib version: {matplotlib.__version__}")
+print(f"PyTorch version: {torch.__version__}")
