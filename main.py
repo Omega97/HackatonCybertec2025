6ff357db972f4c545e6879429e0ad4289bcbc5f7
Omar Cusma Fait <31404553+Omega97@users.noreply.github.com>
2025-04-05 14:36:27 +0200

    version check

--- main.py
+++ main.py
@@ -8,3 +8,8 @@ print(sys.version)
 print(f"NumPy version: {numpy.__version__}")
 print(f"Matplotlib version: {matplotlib.__version__}")
 print(f"PyTorch version: {torch.__version__}")
+
+# 3.11.5 (tags/v3.11.5:cce6ba9, Aug 24 2023, 14:38:34) [MSC v.1936 64 bit (AMD64)]
+# NumPy version: 2.2.4
+# Matplotlib version: 3.10.1
+# PyTorch version: 2.6.0+cpu
